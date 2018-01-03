1ba85cf43678a19c742e64501354e21610d0dccf
Deepanjan <deepanjan.chakrabarty@student.unsw.edu.au>
2018-01-03 19:34:55 +1100

    Added backprop algorithm. Haven't incorporated activation functions yet

--- src/models.py
+++ src/models.py
@@ -19,10 +19,11 @@ class Neuron:
 
 
 class Layer:
-    def __init__(self, num_neurons, activation=None):
+    def __init__(self, num_neurons, activation=None, learning_rate=None):
         self.neurons = [Neuron() for _ in range(num_neurons)]
         self.connections = None
         self.activation = activation
+        self.learning_rate = learning_rate
 
     def connect_new_layer(self, layer):
         self.connections = LayerConnections({'input':len(layer.neurons),'output':len(self.neurons)})
@@ -36,50 +37,67 @@ class Layer:
         # This for loop goes through the list of neurons and
         # updates their values with the values provided 
         # through the new inputs
-        for i in range(len(output_vec)):
-            self.neurons[i].update_value(output_vec[i])
+        self.update_neuron_values(output_vec)
 
         return output_vec
+    
+    def update_neuron_values(self, vec):
+        for i in range(len(vec)):
+            self.neurons[i].update_value(vec[i])
 
     def get_neuron_values(self):
         return [neuron.get_value() for neuron in self.neurons]
-    
+
+    def backprop_error(self, errors, input_array):
+        weight_matrix = self.connections.weight_matrix
+        updated_errors = np.zeros(weight_matrix.shape)
+        for i in range(weight_matrix.shape[0]):
+            for j in range(weight_matrix.shape[1]):
+                updated_errors[i][j] = errors[i] * input_array[j] 
+                weight_matrix[i][j] -= self.learning_rate * updated_errors[i][j] 
+        
+        summed_updated_errors = np.sum(updated_errors, axis=0)
+        return summed_updated_errors
+
+
+        
     def __repr__(self):
         return "Layer with " + str(len(self.neurons)) + " neurons"
 
 
 class Graph:
-    def __init__(self, num_inputs, error_func=SquaredSum(), learning_rate=0.9):
+    def __init__(self, num_inputs, error_func=SquaredSum(), global_learning_rate=0.9):
         self.layers = [Layer(num_inputs)]
         self.num_layers = 1
-        self.learning_rate = learning_rate
+        self.global_learning_rate = global_learning_rate
         self.error_func = error_func
 
     def add(self, new_layer, weights=None):
         self.layers.append(new_layer)
         self.num_layers += 1
         new_layer.connect_new_layer(self.layers[-2])
+        if not new_layer.learning_rate:
+            new_layer.learning_rate = self.global_learning_rate
 
     def __repr__(self):
         return "Network with " + str(self.num_layers) + " layers.\n" + \
                 "\n".join([str(layer) for layer in self.layers])
 
-    def feed(self, inputs):
+    def feed(self, inputs, target_outputs):
         outputs = np.array(inputs).transpose()
+
+        self.layers[0].update_neuron_values(outputs)
+
         for layer in self.layers[1:]:
             outputs = layer.feed_forward(outputs)
-
+        
+        target_outputs = np.array(target_outputs)
+        self.correct(outputs, target_outputs)
         print(outputs)
 
     def correct(self, outputs, target_outputs):
-        for in_layer, out_layer in zip(reversed(self.layers[1:]), reversed(self.layers[:-1])):
-            out_layer.backprop_error(in_layer.get_neuron_values())
-
-
-out - target
-
-
-
-
+        errors = outputs - target_outputs
+        for in_layer, out_layer in zip(reversed(self.layers[:-1]), reversed(self.layers[1:])):
+            errors = out_layer.backprop_error(errors, in_layer.get_neuron_values())
 
 
--- src/run.py
+++ src/run.py
@@ -2,11 +2,12 @@ from models import *
 from activations import *
 
 def main():
-    graph = Graph(2)
+    graph = Graph(3)
     graph.add(Layer(3, activation=Sigmoid()))
     graph.add(Layer(3, activation=Sigmoid()))
     graph.add(Layer(2, activation=Sigmoid()))
-    graph.feed([1,1])
+    for i in range(1000):
+        graph.feed([1,1,1],[0,1])
     print(graph)
 
 if __name__=="__main__":
